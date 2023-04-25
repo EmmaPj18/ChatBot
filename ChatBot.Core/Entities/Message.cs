5d5903a28aae85af01b5157112697b4f5e41ed3e
Emmanuel Polanco Jimenez <emmapj18@gmail.com>
2023-04-24 21:14:29 -0400

    Use RabbitMq to integrate bot with chat app

--- ChatBot.Core/Entities/Message.cs
+++ ChatBot.Core/Entities/Message.cs
@@ -2,7 +2,7 @@
 
 public sealed class ChatMessage
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
     public required string Message { get; set; }
     public DateTimeOffset SendAt { get; set; }
     public int UserId { get; set; }
